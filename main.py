5057282f13f22267f1981a81abc778e64be2707b
Asher Sonntag <asonntag2024@ravenscroft.org>
2021-03-05 18:01:21 +0000

    Able to hold down keys

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ running = True
 
 GREEN = (0,255,0)
 RED = (255,0,0)
-FPS = 60
+FPS = 120
 FPSCLOCK = pygame.time.Clock()
 DISPLAYSURF = pygame.display.set_mode((400,400))
 color = RED
 while running:
-  color = RED
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
-      pygame.quit()
       sys.exit()
-    if event.key == pygame.K_SPACE:
-      color = GREEN
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_SPACE: 
+        color = GREEN
+    elif event.type == pygame.KEYUP:
+      if event.key == pygame.K_SPACE:
+        color = RED
   DISPLAYSURF.fill(color)
   pygame.display.update()
   FPSCLOCK.tick(FPS)
